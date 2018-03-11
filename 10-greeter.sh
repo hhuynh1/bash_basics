5785676a3e1291fade650b5fffadbb32680d4964
Henry Huynh <henryhuynh404@gmail.com>
2018-03-11 18:33:32 -0400

    Assignment 6 : 10-Greeter.sh

--- 10-greeter.sh
+++ 10-greeter.sh
@@ -4,23 +4,26 @@
 
 function weekend_greet 
 {
-   echo "Enjoy the weekend! ${1}!"
+   echo "Enjoy the weekend! ${1} ${2}!"
 }
 
 function weekday_greet
 {
-   echo "Hope you are working hard, ${1}!"
+   echo "Hope you are working hard, ${1} ${2}!"
 }
 
 
 echo "Please enter your name"
 read name
 
+echo "Please enter your last name"
+read lastName
+
 day=$(date | cut -d' ' -f1)  # you can use this form instead of backticks
 
 if [ $day = "Sat" ] || [ $day = "Sun" ]; then
-   weekend_greet $name
+   weekend_greet $name $lastName
 else
-   weekday_greet $name
+   weekday_greet $name $lastName
 fi
 
